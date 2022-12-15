fe04e1b28e7c800b0a60bab04eb2e67f8dafb9f8
leeleejueun <118143272+leeleejueun@users.noreply.github.com>
2022-12-15 18:22:43 +0900

    2022/12/15

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $('body').prepend('<header>');
-$('body').append('<footer>');
+$('main').append('<div class="footer">');
 
-$('header').load('./inc.html header',head);
-$('footer').load('./inc.html footer');
+$('header').load('./inc.html header > div', head);
+$('.footer').load('./inc.html .footer >div');
 
 let idx = localStorage.idx || 0;
 
